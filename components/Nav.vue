6d5562a5e2ab699c2e784baaa8f0305c9c0b1f50
dethMastery <owner@detzz.in.th>
2022-01-07 04:16:34 +0700

    adding Nav Part

---
+++ components/Nav.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="nav">
+        <ul>
+            <li>
+                Home
+            </li>
+            <li>
+                App
+            </li>
+            <li>
+                About
+            </li>
+        </ul>
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,5 @@
 <template>
-  
+  <div id="app">
+      <Nav />
+  </div>
 </template>
