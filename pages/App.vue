69b3b50f81d08b28b33b9e6a38b4eca8603cdfb3
dethMastery <owner@detzz.in.th>
2022-01-07 06:09:35 +0700

    styling home

---
+++ pages/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+      <div class="bg"></div>
+      
+      <Nav />
+      <PDevider />
+  </div>
+</template>
+
+<style>
+@import url(./style/main.css);
+@import url(./style/nav.css);
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,16 @@
 <template>
   <div id="app">
-      <div class="bg"></div>
-      
-      <Nav />
-      <PDevider />
-  </div>
+    <div class="bg"></div>
+
+        <Nav />
+        <PDevider />
+
+        <div class="centerize">
+            <h1>
+                <VueClock :isMinute="true" :isSecond="true" />
+            </h1>
+        </div>
+    </div>
 </template>
 
 <style>
@@ -12,3 +18,14 @@
 @import url(./style/nav.css);
 @import url(./style/index.css);
 </style>
+
+<script>
+import Vue from 'vue'
+import VueClock from '@dangvanthanh/vue-clock'
+
+Vue.component('VueClock', VueClock)
+
+export default {
+  components: { VueClock },
+}
+</script>
