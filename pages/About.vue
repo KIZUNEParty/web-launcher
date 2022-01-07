a5417e39b4f23b976cf775739e453af71566facf
dethMastery <owner@detzz.in.th>
2022-01-07 07:16:09 +0700

    adding about page

---
+++ pages/About.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+      <div class="bg"></div>
+      
+      <Nav />
+      <PDevider />
+
+      <div class="centerize">
+        <div class="about">
+            <h1>
+                About!
+            </h1>
+            <p class="ab-text">
+                All of this website is making by <a href="https://detzz.in.th" target="_blank">Suphakit P.</a> by using Nuxt.js which is based on Vue.js ;3
+
+                <br><br><br>
+
+                View Repo on <a href="https://github.com/KIZUNEParty/web-launcher" target="_blank" rel="noopener noreferrer">Github</a>
+            </p>
+            <br>
+            <p class="copyright">
+                &copy; 2021 <a href="http://" target="_blank" rel="noopener noreferrer">Suphakit P.</a> All Right Reserved.
+            </p>
+        </div>
+      </div>
+  </div>
+</template>
+
+<style>
+@import url(./style/main.css);
+@import url(./style/nav.css);
+@import url(./style/about.css);
+</style>
